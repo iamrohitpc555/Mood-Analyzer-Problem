6cc41ee93391f6145ffe34187e601eb1f43f2623
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-14 21:56:34 +0530

    Create MoodAnalysisException.java

---
+++ MoodAnalysisException.java
@@ -0,0 +1,9 @@
+package com.blz.moodanalyser;
+
+@SuppressWarnings("serial")
+public class MoodAnalysisException extends Exception
+{
+	public MoodAnalysisException(String message) {
+		super(message);
+	}
+}
