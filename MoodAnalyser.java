8f9a8ebdd6775256e4bc814f40b73473336e63cb
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-14 21:59:08 +0530

    Update MoodAnalyser.java

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -2,32 +2,11 @@ package com.blz.moodanalyser;
 
 public class MoodAnalyser
 {
-	static String message;
-	
-	public MoodAnalyser() {
-	}
-	
-	public MoodAnalyser(String message)
+	public String analyseMood(String message)
 	{
-		super();
-		MoodAnalyser.message = message;
+		if(message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
 	}
-
-	public static String analyseMood() throws MoodAnalysisException
-	{
-		try
-		{
-			if(message.contains("Sad"))
-				return "SAD";
-		}
-		catch (NullPointerException e)
-		{
-			if (message == null)
-				throw new MoodAnalysisException("Invalid mood. Enter a message");
-			else if(message == "")
-				throw new MoodAnalysisException("This is an empty message. Enter other message");
-		}
-		return "HAPPY";
-	}
-	
 }
