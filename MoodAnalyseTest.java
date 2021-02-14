032bda5a4debff70d45d13274bc2faad42218e29
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-02-14 22:01:35 +0530

    Create MoodAnalyseTest.java

---
+++ MoodAnalyseTest.java
@@ -0,0 +1,25 @@
+package com.blz.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyseTest
+{
+	
+	@Test
+	public void givenMessage_whenSad_shouldReturnSad()
+	{
+		MoodAnalyser moodanalyser = new MoodAnalyser();
+		String mood = moodanalyser.analyseMood("I am in Sad mood");
+		Assert.assertEquals("SAD", mood);
+	}
+	
+	@Test
+	public void givenMessage_whenNotSad_shouldReturnHappy()
+	{
+		MoodAnalyser moodanalyser = new MoodAnalyser();
+		String mood = moodanalyser.analyseMood("I am in Any mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+}
